fb56d7a751116257487697d84fc758fffd418e2a
Muriithi Ian <imuriithiian@gmail.com>
2023-03-16 23:29:44 +0300

    Project initialization: Resource setup with navigation

---
+++ pages/DetailPage.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const DetailPage = ({ route, navigation }) => {
+	return (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>Detail Page</Text>
+		</View>
+	);
+};
+export default DetailPage;
